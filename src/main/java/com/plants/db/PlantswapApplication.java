a01cd2085c81ed3fe066cde6737771774751eb92
yosiris-m <yosiris0510@gmail.com>
2025-01-21 22:24:09 +0100

    initial commit

---
+++ src/main/java/com/plants/db/PlantswapApplication.java
@@ -0,0 +1,13 @@
+package com.plants.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantswapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantswapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plants/db/PlantswapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plants.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantswapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
