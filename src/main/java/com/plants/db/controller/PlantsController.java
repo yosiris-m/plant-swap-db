12023c557143cbd3dc4ccd7df787671b448fbb75
yosiris-m <yosiris0510@gmail.com>
2025-01-23 17:15:49 +0100

    feat: add post plants

--- src/main/java/com/plants/db/controller/PlantsController.java
+++ src/main/java/com/plants/db/controller/PlantsController.java
@@ -1,11 +1,12 @@
 package com.plants.db.controller;
 
 import com.plants.db.models.Plants;
+import com.plants.db.models.Users;
 import com.plants.db.repository.PlantaRepository;
 import com.plants.db.repository.UserRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,4 +31,16 @@ public class PlantsController {
         }
         return plants;
     }
+
+    //http://localhost:8080/api/plants/create?userId=3
+    @PostMapping("/create")
+    public ResponseEntity<Plants> createPlant(@RequestBody Plants plant, @RequestParam int userId) {
+        Users user = userRepository.findById(userId).orElseThrow(()->  new RuntimeException("El usuario no se ha encontrado"));
+
+        plant.setUser(user);
+        Plants savedPlant = plantaRepository.save(plant);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedPlant);
+    }
+
 }
