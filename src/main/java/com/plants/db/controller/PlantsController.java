13da78ed51f4cecd2728b2db9e129ae6b1ec601a
yosiris-m <yosiris0510@gmail.com>
2025-01-23 17:16:34 +0100

    feat: add put y delete plants

--- src/main/java/com/plants/db/controller/PlantsController.java
+++ src/main/java/com/plants/db/controller/PlantsController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/plants")
@@ -41,6 +42,51 @@ public class PlantsController {
         Plants savedPlant = plantaRepository.save(plant);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(savedPlant);
+    }
+
+    //http://localhost:8080/api/plants/1?userId=1
+    @PutMapping("/{id}")
+    public ResponseEntity<Plants> updatePlant(
+            @PathVariable Long id,
+            @RequestBody Plants plant,
+            @RequestParam int userId) {
+
+        // Verificar si el usuario existe
+        Users user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("El usuario no se ha encontrado"));
+
+        // Verificar si la planta existe
+        Optional<Plants> isExistingPlant = plantaRepository.findById(id);
+        if (isExistingPlant.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null); // Planta no encontrada
+        }
+
+        Plants existingPlant = isExistingPlant.get();
+
+        // Verificar si la planta pertenece al usuario
+        if (!Integer.valueOf(existingPlant.getUser().getId()).equals(user.getId())) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null); // La planta no pertenece al usuario
+        }
+
+        // Actualizar solo los campos permitidos
+        existingPlant.setName_plant(plant.getName_plant());
+        existingPlant.setDescription_plant(plant.getDescription_plant());
+        existingPlant.setLocation(plant.getLocation());
+
+        // Guardar los cambios
+        Plants updatedPlant = plantaRepository.save(existingPlant);
+        return ResponseEntity.ok(updatedPlant);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Plants> deletePlant(@PathVariable Long id) {
+        Optional<Plants> isExistingPlant = plantaRepository.findById(id);
+        if (isExistingPlant.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        Plants existingPlant = isExistingPlant.get();
+        plantaRepository.delete(existingPlant);
+        return ResponseEntity.ok(existingPlant);
     }
 
 }
